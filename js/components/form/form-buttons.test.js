1da42a9237e303442aa51c114d20e1c043e4f642
Don Helm <don.helm@eighthnotemedia.com>
2016-02-09 18:06:36 +0000

    add missed files

---
+++ js/components/form/form-buttons.test.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+    describe('form-buttons', function() {
+        beforeEach(window.module(app.name));
+        
+        it('should test properly', function() {
+            expect(false).to.be.true;
+        });
+    });
+}
